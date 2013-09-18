58f3295d92f8dba26da32994091c7809fbe5cd42
theapi <peter_clarke@dennis.co.uk>
2013-09-18 14:22:52 +0100

    hook_reevoomark_sku_alter($sku) for providing the product sku

---
+++ reevoomark.api.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Documents API functions for the ReevooMark module.
+ */
+
+/**
+ * Alter the product sku before the review is requested.
+ *
+ */
+function hook_reevoomark_sku_alter(&$sku) {
+  $menu_object = menu_get_object();
+  if (!empty($menu_object->field_sku))) {
+    $sku = $menu_object->field_sku;
+  }
+}
