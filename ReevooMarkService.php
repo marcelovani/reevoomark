f68f695017a004870976fc51d7dbf38bde7835f4
theapi <peter_clarke@dennis.co.uk>
2013-09-19 17:19:23 +0100

    Use Drupal caching

--- ReevooMarkService.php
+++ ReevooMarkService.php
@@ -9,6 +9,19 @@ class ReevooMarkService extends ReevooMark implements ReevooMarkServiceInterface
 {
 
   /**
+   * The drupal cache object.
+   *
+   * @var stdClass
+   */
+  protected $cache;
+
+  public function __construct($url, $retailer, $sku)
+  {
+    // Pass FALSE as the $cache parm to the parent library as this uses Drupal caching.
+    parent::__construct(FALSE, $url, $retailer, $sku);
+  }
+
+  /**
    * The overall score for the reviewed product.
    *
    * @return string
@@ -46,6 +59,58 @@ class ReevooMarkService extends ReevooMark implements ReevooMarkServiceInterface
   public function getBody()
   {
     return $this->body();
+  }
+
+  public function getCacheId()
+  {
+    return $this->remote_url;
+  }
+
+  /**
+   * Use Drupal caching not the file based one of the parent library.
+   *
+   * @param string $data
+   */
+  protected function saveToCache($data)
+  {
+    cache_set($this->getCacheId(), $data, 'cache_reevoomark', REQUEST_TIME + 86400);
+  }
+
+  protected function cacheGet()
+  {
+    if (is_null($this->cache)) {
+      $this->cache = cache_get($this->getCacheId(), 'cache_reevoomark');
+    }
+  }
+
+  /**
+   * Load from the Drupal cache.
+   *
+   * @return string
+   */
+  protected function loadFromCache()
+  {
+    if (is_object($this->cache)) {
+      if (REQUEST_TIME < $this->cache->expire) {
+        return $this->cache->data;
+      }
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * The time the document was saved to the cache, if at all.
+   */
+  protected function cacheMTime()
+  {
+    $this->cacheGet();
+
+    if (is_object($this->cache)) {
+      return $this->cache->created;
+    }
+
+    return FALSE;
   }
 
 }
