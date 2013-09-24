bacba57a3e749297af3f81402bae0133155f20e4
theapi <peter_clarke@dennis.co.uk>
2013-09-24 11:05:13 +0100

    Administer ReevooMark settings permission

--- reevoomark.api.php
+++ reevoomark.api.php
@@ -10,7 +10,7 @@
  */
 function hook_reevoomark_sku_alter(&$sku) {
   $menu_object = menu_get_object();
-  if (!empty($menu_object->field_sku))) {
+  if (!empty($menu_object->field_sku)) {
     $sku = $menu_object->field_sku;
   }
 }
